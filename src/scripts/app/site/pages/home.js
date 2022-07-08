ee4fb6d9da4ecbd38c549e89d6fe68de39b90d43
Mureed Sultan <mureedsultangeni@gmail.com>
2022-07-08 11:17:54 +0500

    Tea Animation

--- src/scripts/app/site/pages/home.js
+++ src/scripts/app/site/pages/home.js
@@ -1,17 +1,8 @@
 import getComp from 'setjs/template/component.js';
-import todo from 'app/site/examples/todo.js';
-
-export default {
-  templates: ['site/home'],
-  preload: function({success, error}) {
-    $.get('/templates/todo.html', function(todoTxt) {
-      success(todoTxt);
-   }, 'text').fail(error);
-  },
-  comp: function(opts, todoHtml) {
-    var pageComp = getComp('site/home');
-    pageComp.$todo.append(todo.comp().$root);
-    pageComp.$todoHtml[0].innerText = todoHtml;
-    return pageComp;
+export default{
+  templates:['site/home'],
+  comp:function(){
+    let page = getComp('site/home');
+    return page;
   }
 };
